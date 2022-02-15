f4170776e0749b88786b77651926c1c7955a8d7a
ynn <1830084621@qq.com>
2022-02-15 22:16:08 +0800

    "2-15 study"

---
+++ front-Study/2-15/1.js
@@ -0,0 +1,5 @@
+//'...'这个扩展运算符可以帮助我们把一系列的入参变为数组
+function readArr(...args) {
+    console.log(args)
+}
+readArr(1,2,3)
---
+++ front-Study/2-15/2.js
@@ -0,0 +1,6 @@
+var datas = [10,20,30];
+datas.unshift(40,50);
+datas.pop();
+datas.push(60,70);
+datas.shift();
+console.log(datas.toString());
