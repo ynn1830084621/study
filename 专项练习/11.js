ef0cbdb70cef440d3499ef4870b48411136a2b17
ynn <1830084621@qq.com>
2022-03-14 23:43:21 +0800

    "题目补充"

---
+++ 专项练习/11.js
@@ -0,0 +1,12 @@
+Promise.resolve()
+.then(() => {
+    console.log('p1');
+}).then(() => {
+    console.log('p2');
+})
+process.nextTick(() => {
+    console.log('n1');
+    process.nextTick(() => {
+        console.log('n2');
+    })
+})
