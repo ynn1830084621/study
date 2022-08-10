d8a0bee9c825d9ac2f6fbdc04de74ee717fe830b
ynn1830084621 <1830084621@qq.com>
2022-08-10 22:33:58 +0800

    ‘’

--- 算法/数据结构/栈/回文数/1.js
+++ 算法/数据结构/栈/回文数/1.js
@@ -1,17 +1,18 @@
 var isPalindrome = function(x) {
     let word = x.toString()
     let rword = '';
-    console.log(word.length);
+    // console.log(word.length);
     for (let i = word.length - 1; i >= 0; i--) {
         rword += word[i]
     }
-    console.log(word);
-    console.log(rword);
-    if (word == rword) {
-        return true
-    } else {
-        return false
-    }
+    // console.log(word);
+    // console.log(rword);
+    // if (word == rword) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+    return word === rword
 };
 
-console.log(isPalindrome(123));
+console.log(isPalindrome(11));
