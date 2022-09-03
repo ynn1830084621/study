7bd0a0c05975320b44f9d0632e15b80afe81727b
ynn1830084621 <1830084621@qq.com>
2022-09-03 20:45:57 +0800

    “补充”

--- 闭包/11.js
+++ 闭包/11.js
@@ -1,2 +1,14 @@
+// function generateName(prefix, type, itemName) {
+//     return prefix + type + itemName
+// }
 
 
+
+// 函数柯里化
+function generateName(prefix) {  
+    return function(type) {
+        return function (itemName) {
+            return prefix + type + itemName
+        }    
+    }
+}
