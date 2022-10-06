60d9a9a2e5d1b59d5f65c900b650aed7675fd6f6
ynn1830084621 <1830084621@qq.com>
2022-10-06 22:48:11 +0800

    ’‘

--- JS例子/7.js
+++ JS例子/7.js
@@ -4,4 +4,7 @@ let b = ['3', '4', '5'].map(parseInt);
 console.log(b);//[ 3, NaN, NaN ]
 
 
+// arr.map(function callback(currentValue[, index[, array]])
+// callback一共可以接收三个参数，其中第一个参数代表当前被处理的元素，而第二个参数代表该元素的索引。
 //parseInt(string, radix)
+// 接收两个参数，第一个表示被处理的值（字符串），第二个表示为解析时的基数。
