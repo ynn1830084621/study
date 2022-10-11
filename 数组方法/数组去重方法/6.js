0b141a909dcc4827f80b70e9f2316362c0992b76
ynn1830084621 <1830084621@qq.com>
2022-10-11 22:53:53 +0800

    "补充数组去重"

---
+++ 数组方法/数组去重方法/6.js
@@ -0,0 +1,13 @@
+function unique(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[i] === arr[j]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
+}
+
+let result = unique([1,1,2,2,3,3,4,4,5,5])
+console.log(result);//[ 1, 2, 3, 4, 5 ]
