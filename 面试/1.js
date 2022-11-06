54aa4405ff1019cd7da8eae2b2e6888db8faa3c7
ynn1830084621 <1830084621@qq.com>
2022-11-06 23:21:00 +0800

    "面试题"

---
+++ 面试/1.js
@@ -0,0 +1,9 @@
+console.log(`${(x => x)('hello')} js`);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(undefined == null);
+
+var a = '0';
+function a() {
+}
+console.log(typeof a);//string
