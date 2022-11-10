c782c3e721fe62b5c325b235b4dd85786dab0b3c
ynn1830084621 <1830084621@qq.com>
2022-11-10 23:06:07 +0800

    ''

--- 算法/力扣题目/6.js
+++ 算法/力扣题目/6.js
@@ -1,11 +1,14 @@
 var moveZeroes = function(nums) {
-    for(let i = 0; i < nums.length; i++) {
+    let j = 0;
+    for(let i = 0; i < nums.length - j; i++) {
         if(nums[i] === 0) {
             nums.push(0)
             nums.splice(i, 1);
+            j++;
+            i--
         }
     }
     return nums
 };
-console.log(moveZeroes([0,0,1]));//[0, 1, 0]
+console.log(moveZeroes([0,0,1]));//[1, 0, 0]
 
