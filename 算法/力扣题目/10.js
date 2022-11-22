7048624ef813ca6b806285bb39c75f23ac8c969d
ynn1830084621 <1830084621@qq.com>
2022-11-22 23:38:39 +0800

    ''

---
+++ 算法/力扣题目/10.js
@@ -0,0 +1,13 @@
+var largestNumber = function(nums) {
+    nums.sort((a, b) => {
+        return ('' + b + a) - ('' + a + b);
+    })
+
+    if(nums[0] === 0) {
+        return '0';
+    }
+    
+    return nums.join('');
+
+};
+console.log(largestNumber([1, 2]));//21
