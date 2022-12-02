09938e5343855a30dd5ba88c8a14aea384e122a0
ynn1830084621 <1830084621@qq.com>
2022-12-02 23:17:38 +0800

    '整数反转'

---
+++ 算法/力扣题目/15.js
@@ -0,0 +1,13 @@
+var reverse = function(x) {
+    let rev = 0;
+    while (x !== 0) {
+        const digit = x % 10;
+        x = parseInt(x / 10);
+        rev = rev * 10 + digit;
+        if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
+            return 0;
+        }
+    }
+    return rev;
+};
+console.log(reverse(123));//321
