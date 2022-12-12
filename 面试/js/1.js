754cad1142d5a17f622dafc1f5e77dd8b90e6054
ynn1830084621 <1830084621@qq.com>
2022-12-12 21:58:37 +0800

    ''

--- 面试/js/1.js
+++ 面试/js/1.js
@@ -1,7 +1,8 @@
-console.log(`${(x => x)('hello')} js`);
-console.log(null == 0);
-console.log(undefined == 0);
-console.log(undefined == null);
+console.log(`${(x => x)('hello')} js`);//hello js
+console.log(null == 0);// false
+console.log(undefined == 0);//false
+console.log(undefined == null);//true
+console.log(typeof NaN === 'number');//true
 
 var a = '0';
 function a() {
