17641569afbbc7a2a893e762c2ef5c010f41bd6f
ynn1830084621 <1830084621@qq.com>
2022-12-18 22:47:55 +0800

    ''

--- es6/set和map数据结构/Map/4.js
+++ es6/set和map数据结构/Map/4.js
@@ -4,3 +4,16 @@ const hello = function() {console.log('hello');};
 m.set(hello, 'Hello ES6!') // 键是函数
 
 console.log(m.get(hello));  // Hello ES6!
+
+
+
+const set = new Set([
+    ['foo', 1],
+    ['bar', 2]
+]);
+const m1 = new Map(set);
+console.log(m1.get('foo')); // 1
+
+const m2 = new Map([['baz', 3]]);
+const m3 = new Map(m2);
+console.log(m3.get('baz'));// 3
