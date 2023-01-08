ae394bd7b82265cd8c1ebc28014eeb81ef73d786
ynn1830084621 <1830084621@qq.com>
2023-01-08 23:03:21 +0800

    ''

--- 面试/js/1.js
+++ 面试/js/1.js
@@ -3,6 +3,8 @@ console.log(null == 0);// false
 console.log(undefined == 0);//false
 console.log(undefined == null);//true
 console.log(typeof NaN === 'number');//true
+console.log({} + {});//[object Object][object Object]
+//先进行 ToPrimitive(obj, String)转换得到原始值，在进行ToString转换为字符串
 
 var a = '0';
 function a() {
